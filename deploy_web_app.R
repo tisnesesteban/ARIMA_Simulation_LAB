7c4cf7772223f3cd74073b780a36df5d0ee11da2
tisnesesteban <tisnesesteban@gmail.com>
2025-10-31 18:52:54 -0300

    Implement web app deployment with rsconnect
    Added code to deploy the web application using rsconnect.

--- deploy_web_app.R
+++ deploy_web_app.R
@@ -1,2 +1,10 @@
+rm(list=ls())
+library(rsconnect)
+
+rsconnect::setAccountInfo(name='', token='', 
+                          secret='')
+
+rsconnect::deployApp('/Users/estebantisnes/Documents/GitHub/ARIMA_Simulation_LAB/', 
+                     account = "tisnesesteban")
 
 
