d3b4bb87c8c2ecf2e6701bae0beb010aa9e72fae
tisnesesteban <tisnesesteban@gmail.com>
2025-10-31 18:51:06 -0300

    Update deploy_web_app.R

--- deploy_web_app.R
+++ deploy_web_app.R
@@ -1,10 +1,2 @@
 
-rm(list=ls())
-library(rsconnect)
-
-rsconnect::setAccountInfo(name='tisnesesteban', token='77768124592EB05F595F0A2CBE44636C', 
-                          secret='cjVawps2PWok3PNgSeqefViRF+dCN0OgSe7Jd8TT')
-
-rsconnect::deployApp('/Users/estebantisnes/Documents/GitHub/ARIMA_Simulation_LAB/', 
-                     account = "tisnesesteban")
 
